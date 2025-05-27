da763c7118ba0968a2bebededf527f95eb352799
Akshat Jain <79906349+Akshat-Jain1603@users.noreply.github.com>
2025-05-27 13:08:40 +0530

    Update calc_marks.py

--- calc_marks.py
+++ calc_marks.py
@@ -6,7 +6,8 @@ import requests
 def calc_marks(url, answer_key):
     headers = {
     "Accept-Language": "en-US,en;q=0.9",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",}
+    "User-Agent": '''Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36
+sec-ch-ua: "Google Chrome";v="137", "Chromium";v="137", "Not/A)Brand";v="24"'''}
     
     legend = {'A':0,'B':1,'C':2,'D':3}
     response = requests.get(url, headers = headers).content
